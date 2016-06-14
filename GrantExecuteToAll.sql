8741b67721cd439926d730af17100d00c7b0c2c7
Neil Pullinger <ncpullinger+github@gmail.com>
2016-06-14 21:34:41 +0100

    Adding GRANT EXECUTE code

---
+++ GrantExecuteToAll.sql
@@ -0,0 +1,6 @@
+﻿-- This routine writes out GRANT EXECUTE statements you can then run
+SELECT 'GRANT EXECUTE ON ' + NAME + ' TO mylogin'
+FROM SYSOBJECTS
+WHERE TYPE = 'P'
+AND LEFT(NAME,2) <> 'sp'
+AND LEFT(NAME,2) <> 'dt'
