ca009d5513e137e5a03bd590f71f4d1b1a49715b
Neil Pullinger <ncpullinger+github@gmail.com>
2016-06-14 21:32:29 +0100

    Adding restore SQL code.

---
+++ Restore.sql
@@ -0,0 +1,10 @@
+﻿RESTORE DATABASE myDatabase
+FROM DISK = 'D:\myDatabase.bak'
+WITH REPLACE,
+MOVE ‘abc_db' TO 'D:\abc_db.mdf',
+MOVE 'abc_db_Log' TO 'D:\abc_db_Log.ldf'
+GO
+
+--If you need to check what logical names are, run the following:
+--RESTORE FILELISTONLY
+--FROM DISK = 'D:\myDatabase.bak'
