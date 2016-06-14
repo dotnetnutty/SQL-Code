456badecc44661c44ada1bbc401a7b2a2f9ed82a
Neil Pullinger <ncpullinger+github@gmail.com>
2016-06-14 21:29:41 +0100

    Adding backup code

---
+++ Backup.sql
@@ -0,0 +1,9 @@
+﻿DECLARE @fname NVARCHAR(100)
+SELECT @fname = 'D:\MyBackup-' + CONVERT(varchar,getdate(),112) + '.bak'
+
+BACKUP DATABASE myDatabase
+TO DISK = @fname
+WITH FORMAT,
+NAME = 'myDatabase-Full Database Backup',
+CHECKSUM
+SELECT 'Backed up to ' + @fname
